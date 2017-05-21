18617795169718cf58fe024bf765c3df5d3b25c7
jupabass89 <jupabass89@gmail.com>
2017-05-20 19:47:41 -0500

    Recuepré la clase archivo y las pruebas que estaban

--- PolinomioCromatico/src/Modelo/Pruebas.java
+++ PolinomioCromatico/src/Modelo/Pruebas.java
@@ -1,15 +1,31 @@
 package Modelo;
 
+import Modelo.grafos.LinkedAdyListG;
 import java.awt.FlowLayout;
 import java.io.File;
+import java.io.IOException;
 import javax.swing.JFrame;
 
 public class Pruebas {
     
     
-    public static void main(String[] args) {
-//    
- 
+    public static void main(String[] args) throws IOException {
+
+        JFrame j = new JFrame();
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        j.setLayout(new FlowLayout());
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Archivo a = new Archivo();
+        a.setFile(j);
+        
+        File f = a.getFile();
+        LinkedAdyListG l = new LinkedAdyListG(f);
+        l.completo();
+        System.out.println("--------------");
+        l.imprimirGrafo();       
+        System.out.println("-------");
+      
+
     }
 
 }
