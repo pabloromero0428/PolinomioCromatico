65ef77bd1f8813ae6672e1f800c57e6bfa6dd2a3
JuanPablo <JuanPablo@192.168.1.12>
2017-05-20 19:50:21 -0500

    Guarrga archivo

--- PolinomioCromatico/src/Controlador/ControlPolyC.java
+++ PolinomioCromatico/src/Controlador/ControlPolyC.java
@@ -2,7 +2,9 @@ package Controlador;
 import Modelo.PolyC;
 import Modelo.grafos.LinkedAdyListG;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import modelo.Polynom;
 
 public class ControlPolyC {
@@ -23,7 +25,22 @@ public class ControlPolyC {
     }
 
     public void guardarResultados(String path) {
-  
+      try {
+            long time_start, time_end, time_total;
+            time_start = System.currentTimeMillis();         
+            time_end = System.currentTimeMillis();
+            time_total = time_end - time_start;           
+            FileWriter fw = new FileWriter("Registro Asegurados.txt", true); // abro el archivo de lectura
+            PrintWriter pw = new PrintWriter(fw);   // abro el archivo de escritura
+            //separo los campos dentro del archivo
+            pw.println("T: "+ time_total + " P(G,x): " + polynom);
+            //cierro los archivos de lectura y escritura
+
+            pw.close();
+            fw.close();
+        } catch (IOException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
     
     }
 
